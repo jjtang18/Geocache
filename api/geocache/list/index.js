7cce029888fe38e3df1afb349fdfb1ccc00472d5
Joseph Tang <josephtang724@gmail.com>
2020-10-04 22:49:26 -0700

    date can be displayed, postman is failing

--- api/geocache/list/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('/Users/josephtang/mblapp/Geocache/server/util/configureApi.js');
-
-app.post('*', (req, res) => {
-    res.status(200).json({
-        result: {},
-    });
-});
-
-module.exports = app;
