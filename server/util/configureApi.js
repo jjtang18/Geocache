7835f0b30c674ed82a4170a00670abf969d60504
Joseph Tang <josephtang724@gmail.com>
2020-10-06 00:26:25 -0700

    IT FINALLY WORKS

--- server/util/configureApi.js
+++ server/util/configureApi.js
@@ -8,12 +8,4 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('*', (req, res) => {
-    res.status(200).json({
-        result: [],
-        okay: true
-    });
-});
-
 module.exports = app;
