4e071eed021e7ff9002c736cf0232b5562c73fd5
Joseph Tang <josephtang724@gmail.com>
2020-10-11 21:44:31 -0700

    fyj

--- mobile/App/util/api.js
+++ mobile/App/util/api.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+
 const BASE_URL = 'https://geocache.jjtang18.vercel.app';
-=======
-const BASE_URL = 'http://localhost:3000';
->>>>>>> parent of e36166d... Finalized server and app
 
 export const geoFetch = (path, options = {}) => {
     return fetch(`${BASE_URL}/api${path}`, options).then(res => {
