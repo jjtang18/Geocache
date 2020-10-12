6ecbc84cf446ed2a1674e05d93896379b24d38bd
Joseph Tang <josephtang724@gmail.com>
2020-10-11 21:04:38 -0700

    I want to fucking die

---
+++ mobile/App/util/api.js
@@ -0,0 +1,18 @@
+const BASE_URL = 'https://geocache.jjtang18.vercel.app';
+
+export const geoFetch = (path, options = {}) => {
+    return fetch(`${BASE_URL}/api${path}`, options)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+
+            throw new Error('Something went wrong... please try again.');
+        })
+        .catch(error => {
+            // Log to sentry
+            console.warn('ERROR: ', `${BASE_URL}/api${path}`, error);
+
+            throw new Error(error);
+        });
+};
